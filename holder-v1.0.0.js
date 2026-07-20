daf0827c597b3df25228441d5b324e159fc6f186
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2026-07-20 15:27:30 -0700

    feat: holder v1.0.0

---
+++ holder-v1.0.0.js
@@ -0,0 +1,360 @@
+const seg_g0_path0 = () => draw()
+  .movePointerTo([0.01, 17]) // m
+  .ellipseTo([-0.199, 16.801], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-0.199, 13.789]) // v
+  .lineTo([-1.242, 13.734]) // l
+    .lineTo([-1.557, 16.729]) // l
+  .ellipseTo([-1.787, 16.906], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-3.523, 16.631]) // l
+  .ellipseTo([-3.689, 16.391], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-3.062, 13.447]) // L
+    .lineTo([-4.072, 13.176]) // L
+    .lineTo([-5, 16.039]) // L
+  .ellipseTo([-5.262, 16.164], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-6.904, 15.535]) // l
+  .ellipseTo([-7.016, 15.268], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-5.791, 12.516]) // l
+    .lineTo([-6.723, 12.043]) // l
+    .lineTo([-8.227, 14.648]) // l
+  .ellipseTo([-8.51, 14.717], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-9.984, 13.758]) // l
+  .ellipseTo([-10.037, 13.473], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-8.268, 11.039]) // l
+    .lineTo([-9.078, 10.383]) // l
+    .lineTo([-11.092, 12.619]) // l
+  .ellipseTo([-11.383, 12.627], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.627, 11.383]) // l
+  .ellipseTo([-12.619, 11.092], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-10.383, 9.078]) // l
+    .lineTo([-11.039, 8.268]) // l
+    .lineTo([-13.473, 10.037]) // l
+  .ellipseTo([-13.758, 9.984], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-14.717, 8.51]) // l
+  .ellipseTo([-14.648, 8.227], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.043, 6.723]) // l
+    .lineTo([-12.516, 5.791]) // l
+    .lineTo([-15.268, 7.016]) // l
+  .ellipseTo([-15.535, 6.904], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-16.164, 5.262]) // l
+  .ellipseTo([-16.039, 5], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.176, 4.072]) // l
+    .lineTo([-13.447, 3.062]) // l
+    .lineTo([-16.391, 3.689]) // l
+  .ellipseTo([-16.631, 3.523], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-16.906, 1.787]) // l
+  .ellipseTo([-16.729, 1.557], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-13.734, 1.242]) // l
+    .lineTo([-13.789, 0.199]) // l
+  .lineTo([-16.801, 0.199]) // h
+  .ellipseTo([-17, -0.01], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-16.908, -1.766]) // l
+  .ellipseTo([-16.687, -1.955], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-13.693, -1.641]) // l
+    .lineTo([-13.529, -2.672]) // l
+    .lineTo([-16.475, -3.297]) // l
+  .ellipseTo([-16.627, -3.545], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-16.172, -5.242]) // l
+  .ellipseTo([-15.916, -5.381], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-13.053, -4.451]) // l
+    .lineTo([-12.68, -5.426]) // l
+    .lineTo([-15.43, -6.65]) // l
+  .ellipseTo([-15.525, -6.924], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-14.727, -8.49]) // l
+  .ellipseTo([-14.449, -8.572], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-11.842, -7.068]) // l
+    .lineTo([-11.273, -7.943]) // l
+    .lineTo([-13.709, -9.713]) // l
+  .ellipseTo([-13.746, -10], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-12.641, -11.367]) // l
+  .ellipseTo([-12.352, -11.389], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-10.113, -9.375]) // L
+    .lineTo([-9.375, -10.113]) // L
+  .lineTo([-11.389, -12.352]) // l
+  .ellipseTo([-11.367, -12.641], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-10, -13.746]) // L
+  .ellipseTo([-9.713, -13.709], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-7.943, -11.273]) // l
+    .lineTo([-7.068, -11.842]) // l
+    .lineTo([-8.572, -14.449]) // l
+  .ellipseTo([-8.49, -14.727], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-6.924, -15.525]) // l
+  .ellipseTo([-6.65, -15.43], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-5.426, -12.68]) // l
+    .lineTo([-4.451, -13.053]) // l
+    .lineTo([-5.381, -15.916]) // l
+  .ellipseTo([-5.242, -16.172], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-3.545, -16.627]) // l
+  .ellipseTo([-3.297, -16.475], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-2.672, -13.529]) // l
+    .lineTo([-1.641, -13.693]) // l
+    .lineTo([-1.955, -16.688]) // l
+  .ellipseTo([-1.766, -16.908], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-0.01, -17]) // L
+  .ellipseTo([0.199, -16.801], 0.2, 0.2, 0, false, true) // a
+  .lineTo([0.199, -13.789]) // v
+  .lineTo([1.242, -13.734]) // l
+    .lineTo([1.557, -16.729]) // l
+  .ellipseTo([1.787, -16.906], 0.2, 0.2, 0, false, true) // A
+  .lineTo([3.523, -16.631]) // L
+  .ellipseTo([3.689, -16.391], 0.2, 0.2, 0, false, true) // A
+  .lineTo([3.063, -13.447]) // L
+    .lineTo([4.072, -13.176]) // L
+    .lineTo([5, -16.039]) // L
+  .ellipseTo([5.262, -16.164], 0.2, 0.2, 0, false, true) // a
+  .lineTo([6.904, -15.535]) // L
+  .ellipseTo([7.016, -15.268], 0.2, 0.2, 0, false, true) // A
+  .lineTo([5.791, -12.516]) // l
+    .lineTo([6.723, -12.043]) // l
+    .lineTo([8.227, -14.648]) // l
+  .ellipseTo([8.51, -14.717], 0.2, 0.2, 0, false, true) // a
+  .lineTo([9.984, -13.758]) // L
+  .ellipseTo([10.037, -13.473], 0.2, 0.2, 0, false, true) // a
+  .lineTo([8.268, -11.039]) // l
+    .lineTo([9.078, -10.383]) // l
+    .lineTo([11.092, -12.619]) // l
+  .ellipseTo([11.383, -12.627], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.627, -11.383]) // l
+  .ellipseTo([12.619, -11.092], 0.2, 0.2, 0, false, true) // a
+  .lineTo([10.383, -9.078]) // l
+    .lineTo([11.039, -8.268]) // l
+    .lineTo([13.473, -10.037]) // l
+  .ellipseTo([13.758, -9.984], 0.2, 0.2, 0, false, true) // A
+  .lineTo([14.717, -8.51]) // L
+  .ellipseTo([14.648, -8.227], 0.2, 0.2, 0, false, true) // A
+  .lineTo([12.043, -6.723]) // L
+    .lineTo([12.516, -5.791]) // L
+    .lineTo([15.268, -7.016]) // L
+  .ellipseTo([15.535, -6.904], 0.2, 0.2, 0, false, true) // A
+  .lineTo([16.164, -5.262]) // L
+  .ellipseTo([16.039, -5], 0.2, 0.2, 0, false, true) // A
+  .lineTo([13.176, -4.072]) // L
+    .lineTo([13.447, -3.063]) // L
+    .lineTo([16.391, -3.689]) // L
+  .ellipseTo([16.631, -3.523], 0.2, 0.2, 0, false, true) // A
+  .lineTo([16.906, -1.787]) // L
+  .ellipseTo([16.729, -1.557], 0.2, 0.2, 0, false, true) // A
+  .lineTo([13.734, -1.242]) // l
+    .lineTo([13.789, -0.199]) // l
+  .lineTo([16.801, -0.199]) // h
+  .lineTo([16.801, 0.199]) // v
+  .lineTo([13.6, 0.199]) // h
+  .ellipseTo([13.4, 0.01], 0.2, 0.2, 0, false, true) // a
+  .lineTo([13.326, -1.412]) // l
+  .ellipseTo([13.504, -1.621], 0.2, 0.2, 0, false, true) // a
+  .lineTo([16.477, -1.934]) // l
+    .lineTo([16.27, -3.254]) // l
+    .lineTo([13.344, -2.633]) // l
+  .ellipseTo([13.109, -2.775], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.74, -4.15]) // l
+  .ellipseTo([12.873, -4.393], 0.2, 0.2, 0, false, true) // a
+  .lineTo([15.715, -5.316]) // l
+    .lineTo([15.238, -6.564]) // l
+    .lineTo([12.506, -5.35]) // l
+  .ellipseTo([12.246, -5.441], 0.2, 0.2, 0, false, true) // a
+  .lineTo([11.6, -6.709]) // l
+  .ellipseTo([11.678, -6.973], 0.2, 0.2, 0, false, true) // a
+  .lineTo([14.268, -8.467]) // l
+    .lineTo([13.539, -9.59]) // l
+    .lineTo([11.121, -7.832]) // l
+  .ellipseTo([10.848, -7.867], 0.2, 0.2, 0, false, true) // a
+  .lineTo([9.951, -8.975]) // L
+  .ellipseTo([9.973, -9.248], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.195, -11.248]) // L
+    .lineTo([11.248, -12.195]) // L
+    .lineTo([9.248, -9.973]) // L
+  .ellipseTo([8.975, -9.951], 0.2, 0.2, 0, false, true) // A
+  .lineTo([7.867, -10.848]) // L
+  .ellipseTo([7.832, -11.121], 0.2, 0.2, 0, false, true) // a
+  .lineTo([9.59, -13.539]) // l
+    .lineTo([8.467, -14.268]) // l
+    .lineTo([6.973, -11.678]) // l
+  .ellipseTo([6.709, -11.6], 0.2, 0.2, 0, false, true) // a
+  .lineTo([5.441, -12.246]) // l
+  .ellipseTo([5.35, -12.506], 0.2, 0.2, 0, false, true) // a
+  .lineTo([6.564, -15.238]) // L
+    .lineTo([5.316, -15.715]) // L
+    .lineTo([4.393, -12.873]) // L
+  .ellipseTo([4.15, -12.74], 0.2, 0.2, 0, false, true) // A
+  .lineTo([2.775, -13.109]) // l
+  .ellipseTo([2.633, -13.344], 0.2, 0.2, 0, false, true) // A
+  .lineTo([3.254, -16.27]) // l
+    .lineTo([1.934, -16.477]) // l
+    .lineTo([1.621, -13.504]) // l
+  .ellipseTo([1.412, -13.326], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.01, -13.4]) // l
+  .ellipseTo([-0.199, -13.6], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-0.199, -16.588]) // v
+  .lineTo([-1.535, -16.52]) // l
+    .lineTo([-1.223, -13.545]) // l
+  .ellipseTo([-1.391, -13.328], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-2.797, -13.105]) // l
+  .ellipseTo([-3.023, -13.262], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-3.646, -16.186]) // l
+    .lineTo([-4.936, -15.84]) // l
+    .lineTo([-4.014, -12.996]) // l
+  .ellipseTo([-4.131, -12.748], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-5.461, -12.236]) // l
+  .ellipseTo([-5.715, -12.342], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-6.93, -15.074]) // l
+    .lineTo([-8.121, -14.467]) // l
+    .lineTo([-6.627, -11.879]) // l
+  .ellipseTo([-6.691, -11.609], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-7.885, -10.836]) // l
+  .ellipseTo([-8.156, -10.885], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-9.914, -13.303]) // l
+    .lineTo([-10.951, -12.463]) // l
+    .lineTo([-8.951, -10.24]) // l
+  .ellipseTo([-8.959, -9.965], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-9.965, -8.959]) // L
+  .ellipseTo([-10.24, -8.951], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.463, -10.951]) // l
+    .lineTo([-13.303, -9.914]) // l
+    .lineTo([-10.885, -8.156]) // l
+  .ellipseTo([-10.836, -7.885], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-11.609, -6.691]) // l
+  .ellipseTo([-11.879, -6.627], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-14.467, -8.121]) // l
+    .lineTo([-15.074, -6.93]) // l
+    .lineTo([-12.342, -5.715]) // l
+  .ellipseTo([-12.236, -5.461], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.748, -4.131]) // l
+  .ellipseTo([-12.996, -4.014], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-15.84, -4.936]) // l
+    .lineTo([-16.186, -3.646]) // l
+    .lineTo([-13.262, -3.023]) // l
+  .ellipseTo([-13.105, -2.797], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-13.328, -1.391]) // l
+  .ellipseTo([-13.545, -1.223], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-16.52, -1.535]) // l
+    .lineTo([-16.588, -0.199]) // l
+  .lineTo([-13.6, -0.199]) // h
+  .ellipseTo([-13.4, -0.01], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-13.326, 1.412]) // l
+  .ellipseTo([-13.504, 1.621], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-16.477, 1.934]) // l
+    .lineTo([-16.27, 3.254]) // l
+    .lineTo([-13.344, 2.633]) // l
+  .ellipseTo([-13.109, 2.775], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.74, 4.15]) // l
+  .ellipseTo([-12.873, 4.393], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-15.715, 5.316]) // l
+    .lineTo([-15.238, 6.564]) // l
+    .lineTo([-12.506, 5.35]) // l
+  .ellipseTo([-12.246, 5.441], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-11.6, 6.709]) // l
+  .ellipseTo([-11.678, 6.973], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-14.268, 8.467]) // l
+    .lineTo([-13.539, 9.59]) // l
+    .lineTo([-11.121, 7.832]) // l
+  .ellipseTo([-10.848, 7.867], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-9.951, 8.975]) // l
+  .ellipseTo([-9.973, 9.248], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.195, 11.248]) // l
+    .lineTo([-11.248, 12.195]) // l
+    .lineTo([-9.248, 9.973]) // l
+  .ellipseTo([-8.975, 9.951], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-7.867, 10.848]) // l
+  .ellipseTo([-7.832, 11.121], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-9.59, 13.539]) // l
+    .lineTo([-8.467, 14.268]) // l
+    .lineTo([-6.973, 11.678]) // l
+  .ellipseTo([-6.709, 11.6], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-5.441, 12.246]) // l
+  .ellipseTo([-5.35, 12.506], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-6.564, 15.238]) // l
+    .lineTo([-5.316, 15.715]) // l
+    .lineTo([-4.393, 12.873]) // l
+  .ellipseTo([-4.15, 12.74], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-2.775, 13.109]) // l
+  .ellipseTo([-2.633, 13.344], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-3.254, 16.27]) // l
+    .lineTo([-1.934, 16.477]) // l
+    .lineTo([-1.621, 13.504]) // l
+  .ellipseTo([-1.412, 13.326], 0.2, 0.2, 0, false, true) // a
+  .lineTo([0.01, 13.4]) // l
+  .ellipseTo([0.199, 13.6], 0.2, 0.2, 0, false, true) // a
+  .lineTo([0.199, 16.588]) // v
+  .lineTo([1.535, 16.52]) // l
+    .lineTo([1.223, 13.545]) // l
+  .ellipseTo([1.391, 13.328], 0.2, 0.2, 0, false, true) // a
+  .lineTo([2.797, 13.105]) // l
+  .ellipseTo([3.023, 13.262], 0.2, 0.2, 0, false, true) // a
+  .lineTo([3.646, 16.186]) // l
+    .lineTo([4.936, 15.84]) // l
+    .lineTo([4.014, 12.996]) // l
+  .ellipseTo([4.131, 12.748], 0.2, 0.2, 0, false, true) // a
+  .lineTo([5.461, 12.236]) // l
+  .ellipseTo([5.715, 12.342], 0.2, 0.2, 0, false, true) // a
+  .lineTo([6.93, 15.074]) // l
+    .lineTo([8.121, 14.467]) // l
+    .lineTo([6.627, 11.879]) // l
+  .ellipseTo([6.691, 11.609], 0.2, 0.2, 0, false, true) // a
+  .lineTo([7.885, 10.836]) // l
+  .ellipseTo([8.156, 10.885], 0.2, 0.2, 0, false, true) // a
+  .lineTo([9.914, 13.303]) // l
+    .lineTo([10.951, 12.463]) // l
+    .lineTo([8.951, 10.24]) // l
+  .ellipseTo([8.959, 9.965], 0.2, 0.2, 0, false, true) // a
+  .lineTo([9.965, 8.959]) // l
+  .ellipseTo([10.24, 8.951], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.463, 10.951]) // l
+    .lineTo([13.303, 9.914]) // l
+    .lineTo([10.885, 8.156]) // l
+  .ellipseTo([10.836, 7.885], 0.2, 0.2, 0, false, true) // a
+  .lineTo([11.609, 6.691]) // l
+  .ellipseTo([11.879, 6.627], 0.2, 0.2, 0, false, true) // a
+  .lineTo([14.467, 8.121]) // l
+    .lineTo([15.074, 6.93]) // l
+    .lineTo([12.342, 5.715]) // l
+  .ellipseTo([12.236, 5.461], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.748, 4.131]) // l
+  .ellipseTo([12.996, 4.014], 0.2, 0.2, 0, false, true) // a
+  .lineTo([15.84, 4.936]) // l
+    .lineTo([16.186, 3.646]) // l
+    .lineTo([13.262, 3.023]) // l
+  .ellipseTo([13.105, 2.797], 0.2, 0.2, 0, false, true) // a
+  .lineTo([13.328, 1.391]) // l
+  .ellipseTo([13.545, 1.223], 0.2, 0.2, 0, false, true) // a
+  .lineTo([16.729, 1.557]) // l
+    .lineTo([16.687, 1.955]) // l
+    .lineTo([13.693, 1.641]) // l
+    .lineTo([13.529, 2.672]) // l
+    .lineTo([16.475, 3.297]) // l
+  .ellipseTo([16.627, 3.545], 0.2, 0.2, 0, false, true) // a
+  .lineTo([16.172, 5.242]) // l
+  .ellipseTo([15.916, 5.381], 0.2, 0.2, 0, false, true) // a
+  .lineTo([13.053, 4.451]) // l
+    .lineTo([12.68, 5.426]) // l
+    .lineTo([15.43, 6.65]) // l
+  .ellipseTo([15.525, 6.924], 0.2, 0.2, 0, false, true) // a
+  .lineTo([14.727, 8.49]) // l
+  .ellipseTo([14.449, 8.572], 0.2, 0.2, 0, false, true) // a
+  .lineTo([11.842, 7.068]) // l
+    .lineTo([11.273, 7.943]) // l
+    .lineTo([13.709, 9.713]) // l
+  .ellipseTo([13.746, 10], 0.2, 0.2, 0, false, true) // A
+  .lineTo([12.641, 11.367]) // l
+  .ellipseTo([12.352, 11.389], 0.2, 0.2, 0, false, true) // a
+  .lineTo([10.113, 9.375]) // L
+    .lineTo([9.375, 10.113]) // L
+  .lineTo([11.389, 12.352]) // l
+  .ellipseTo([11.367, 12.641], 0.2, 0.2, 0, false, true) // a
+  .lineTo([10, 13.746]) // L
+  .ellipseTo([9.713, 13.709], 0.2, 0.2, 0, false, true) // a
+  .lineTo([7.943, 11.273]) // l
+    .lineTo([7.068, 11.842]) // l
+    .lineTo([8.572, 14.449]) // l
+  .ellipseTo([8.49, 14.727], 0.2, 0.2, 0, false, true) // a
+  .lineTo([6.924, 15.525]) // l
+  .ellipseTo([6.65, 15.43], 0.2, 0.2, 0, false, true) // a
+  .lineTo([5.426, 12.68]) // l
+    .lineTo([4.451, 13.053]) // l
+    .lineTo([5.381, 15.916]) // l
+  .ellipseTo([5.242, 16.172], 0.2, 0.2, 0, false, true) // a
+  .lineTo([3.545, 16.627]) // l
+  .ellipseTo([3.297, 16.475], 0.2, 0.2, 0, false, true) // a
+  .lineTo([2.672, 13.529]) // l
+    .lineTo([1.641, 13.693]) // l
+    .lineTo([1.955, 16.688]) // l
+  .ellipseTo([1.766, 16.908], 0.2, 0.2, 0, false, true) // A
+  .close(); // Z
