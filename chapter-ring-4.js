6426f36b50895a52913596d631ac53309aa1f40d
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2026-07-20 15:27:49 -0700

    feat: chapter ring 2-4

---
+++ chapter-ring-4.js
@@ -0,0 +1,343 @@
+const seg_g1_path1 = () => draw()
+  .movePointerTo([-1.794, 16.9]) // m
+  .cubicBezierCurveTo([-8.063, 14.944], [-3.162, 16.829], [-6.253, 16.045]) // c
+  .ellipseTo([-8.133, 14.674], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-6.387, 11.648]) // l
+  .cubicBezierCurveTo([-6.907, 11.35], [-6.27, 11.446], [-6.79, 11.148]) // c
+  .lineTo([-8.664, 14.368]) // l
+  .ellipseTo([-8.937, 14.441], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([-14.44, 8.938], [-11.116, 13.192], [-13.381, 10.823]) // c
+  .ellipseTo([-14.367, 8.668], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-11.278, 6.863]) // l
+  .cubicBezierCurveTo([-11.576, 6.343], [-11.077, 6.746], [-11.375, 6.225]) // c
+  .lineTo([-14.672, 8.149]) // l
+  .ellipseTo([-14.945, 8.078], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([-16.906, 1.805], [-16.326, 5.706], [-16.705, 3.159]) // c
+  .ellipseTo([-16.566, 1.635], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-15.666, 2.55]) // l
+  .lineTo([-14.45, 2.55]) // h
+  .lineTo([-14.45, 1.45]) // v
+  .lineTo([-14.75, 1.45]) // H
+  .ellipseTo([-14.95, 1.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-14.95, 0.75]) // v
+  .ellipseTo([-14.75, 0.55], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.25, 0.55]) // h
+  .ellipseTo([-13.05, 0.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.05, 1.25]) // v
+  .ellipseTo([-13.25, 1.45], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.55, 1.45]) // h
+  .lineTo([-13.55, 2.55]) // v
+  .lineTo([-12.333, 2.55]) // h
+  .lineTo([-11.45, 1.667]) // l
+  .lineTo([-11.45, -1.667]) // v
+  .lineTo([-12.333, -2.55]) // l
+  .lineTo([-15.667, -2.55]) // h
+  .lineTo([-16.55, -1.667]) // l
+  .lineTo([-16.55, -1.45]) // V
+  .lineTo([-12.75, -1.45]) // H
+  .ellipseTo([-12.55, -1.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-12.55, -0.75]) // v
+  .ellipseTo([-12.75, -0.55], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-16.75, -0.55]) // h
+  .ellipseTo([-16.95, -0.75], 0.2, 0.2, 0, false, true) // A
+    .ellipseTo([-16.75, -0.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-12.95, -0.95]) // h
+  .lineTo([-12.95, -1.05]) // V
+  .lineTo([-16.75, -1.05]) // H
+  .ellipseTo([-16.95, -1.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-16.95, -1.75]) // v
+  .ellipseTo([-16.892, -1.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-15.892, -2.892]) // l
+  .ellipseTo([-15.75, -2.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-12.25, -2.95]) // h
+  .ellipseTo([-12.108, -2.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-11.108, -1.892]) // l
+  .ellipseTo([-11.05, -1.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-11.05, 1.75]) // v
+  .ellipseTo([-11.108, 1.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-12.108, 2.892]) // l
+  .ellipseTo([-12.25, 2.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.75, 2.95]) // h
+  .ellipseTo([-13.95, 2.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.95, 1.25]) // v
+  .ellipseTo([-13.75, 1.05], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-13.45, 1.05]) // h
+  .lineTo([-13.45, 0.95]) // v
+  .lineTo([-14.55, 0.95]) // h
+  .lineTo([-14.55, 1.05]) // V
+  .lineTo([-14.25, 1.05]) // H
+  .ellipseTo([-14.05, 1.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-14.05, 2.75]) // v
+  .ellipseTo([-14.25, 2.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-15.75, 2.95]) // h
+  .ellipseTo([-15.893, 2.891], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-16.385, 2.39]) // l
+  .cubicBezierCurveTo([-14.687, 7.695], [-16.168, 3.761], [-15.785, 5.728]) // c
+  .lineTo([-11.778, 5.997]) // l
+  .cubicBezierCurveTo([-11.076, 7.209], [-11.116, 5.611], [-10.414, 6.822]) // c
+  .lineTo([-13.97, 8.898]) // l
+  .cubicBezierCurveTo([-8.901, 13.98], [-12.951, 10.626], [-10.89, 12.789]) // c
+  .lineTo([-7.253, 11.147]) // l
+  .cubicBezierCurveTo([-6.04, 11.849], [-6.868, 10.485], [-5.657, 11.185]) // c
+  .lineTo([-7.673, 14.677]) // l
+  .cubicBezierCurveTo([-2.401, 16.373], [-6.135, 15.558], [-3.795, 16.184]) // c
+  .lineTo([-2.891, 15.893]) // L
+  .ellipseTo([-2.95, 15.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-2.95, 12.25]) // v
+  .ellipseTo([-2.892, 12.108], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-1.892, 11.108]) // l
+  .ellipseTo([-1.75, 11.05], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.25, 11.05]) // h
+  .ellipseTo([-0.05, 11.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.05, 13.25]) // v
+  .ellipseTo([-0.108, 13.392], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-0.55, 13.833]) // l
+  .lineTo([-0.55, 14.167]) // v
+  .lineTo([-0.167, 14.55]) // l
+  .lineTo([0.167, 14.55]) // h
+  .lineTo([0.55, 14.167]) // l
+  .lineTo([0.55, 13.833]) // v
+  .lineTo([0.108, 13.392]) // l
+  .ellipseTo([0.05, 13.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.05, 11.25]) // v
+  .ellipseTo([0.25, 11.05], 0.2, 0.2, 0, false, true) // A
+  .lineTo([1.75, 11.05]) // h
+  .ellipseTo([1.892, 11.108], 0.2, 0.2, 0, false, true) // a
+  .lineTo([2.892, 12.108]) // l
+  .ellipseTo([2.95, 12.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([2.95, 15.75]) // v
+  .ellipseTo([2.887, 15.896], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([2.438, 16.321], [2.887, 15.896], [2.666, 16.102]) // c
+    .cubicBezierCurveTo([2.396, 16.363], [2.423, 16.336], [2.411, 16.349]) // c
+    .cubicBezierCurveTo([7.712, 14.682], [4.202, 16.122], [6.323, 15.423]) // c
+  .lineTo([6.078, 11.867]) // l
+  .cubicBezierCurveTo([7.29, 11.166], [5.693, 11.204], [6.905, 10.503]) // c
+  .lineTo([8.92, 13.976]) // l
+  .cubicBezierCurveTo([13.979, 8.877], [10.728, 12.84], [12.799, 10.735]) // c
+  .lineTo([11.194, 7.257]) // l
+  .cubicBezierCurveTo([11.896, 6.045], [10.532, 6.871], [11.233, 5.66]) // c
+  .lineTo([14.696, 7.674]) // l
+  .cubicBezierCurveTo([15.98, 4.421], [15.204, 6.81], [15.661, 5.569]) // c
+    .cubicBezierCurveTo([16.373, 2.773], [16.15, 3.814], [16.284, 3.239]) // c
+    .cubicBezierCurveTo([16.429, 2.35], [16.412, 2.571], [16.408, 2.505]) // c
+  .lineTo([15.892, 2.891]) // L
+  .ellipseTo([15.75, 2.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([12.25, 2.95]) // h
+  .ellipseTo([12.108, 2.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([11.108, 1.892]) // l
+  .ellipseTo([11.05, 1.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([11.05, 0.75]) // v
+  .ellipseTo([11.25, 0.55], 0.2, 0.2, 0, false, true) // A
+  .lineTo([12.25, 0.55]) // h
+  .ellipseTo([12.392, 0.608], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.833, 1.05]) // l
+  .lineTo([15.05, 1.05]) // h
+  .lineTo([15.05, 0.95]) // v
+  .lineTo([13.25, 0.95]) // H
+  .ellipseTo([13.108, 0.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.608, 0.392]) // l
+  .ellipseTo([12.55, 0.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([12.55, -0.25]) // v
+  .ellipseTo([12.608, -0.392], 0.2, 0.2, 0, false, true) // a
+  .lineTo([13.108, -0.892]) // l
+  .ellipseTo([13.25, -0.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([15.05, -0.95]) // h
+  .lineTo([15.05, -1.05]) // V
+  .lineTo([12.833, -1.05]) // H
+  .lineTo([12.392, -0.608]) // L
+  .ellipseTo([12.25, -0.55], 0.2, 0.2, 0, false, true) // A
+  .lineTo([11.25, -0.55]) // h
+  .ellipseTo([11.05, -0.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([11.05, -1.75]) // v
+  .ellipseTo([11.108, -1.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.108, -2.892]) // l
+  .ellipseTo([12.25, -2.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([15.75, -2.95]) // h
+  .ellipseTo([15.894, -2.889], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([16.277, -2.499], [15.894, -2.889], [16.074, -2.702]) // c
+    .cubicBezierCurveTo([16.352, -2.429], [16.308, -2.469], [16.32, -2.46]) // c
+    .cubicBezierCurveTo([15.927, -4.613], [16.3, -2.976], [16.206, -3.672]) // c
+    .cubicBezierCurveTo([14.692, -7.697], [15.597, -5.724], [15.142, -6.872]) // c
+  .lineTo([11.806, -6.019]) // l
+  .cubicBezierCurveTo([11.104, -7.23], [11.143, -5.633], [10.442, -6.845]) // c
+  .lineTo([13.985, -8.908]) // l
+  .cubicBezierCurveTo([8.862, -13.991], [12.852, -10.679], [10.814, -12.77]) // c
+  .lineTo([7.254, -11.185]) // l
+  .cubicBezierCurveTo([6.041, -11.884], [6.873, -10.519], [5.66, -11.219]) // c
+  .lineTo([7.65, -14.694]) // l
+  .cubicBezierCurveTo([4.684, -15.897], [6.883, -15.13], [5.782, -15.572]) // c
+    .cubicBezierCurveTo([2.445, -16.341], [3.762, -16.171], [3.046, -16.274]) // c
+  .lineTo([2.892, -15.891]) // L
+  .ellipseTo([2.95, -15.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([2.95, -13.75]) // v
+  .ellipseTo([2.892, -13.608], 0.2, 0.2, 0, false, true) // A
+  .lineTo([2.392, -13.108]) // l
+  .ellipseTo([2.25, -13.05], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.25, -13.05]) // h
+  .ellipseTo([0.05, -13.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.05, -14.75]) // v
+  .ellipseTo([0.25, -14.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.55, -14.95]) // h
+  .lineTo([0.55, -15.05]) // v
+  .lineTo([-0.55, -15.05]) // H
+  .lineTo([-0.55, -14.95]) // V
+  .lineTo([-0.25, -14.95]) // H
+  .ellipseTo([-0.05, -14.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.05, -13.25]) // v
+  .ellipseTo([-0.25, -13.05], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-1.05, -13.05]) // H
+  .lineTo([-1.05, -12.95]) // V
+  .lineTo([2.75, -12.95]) // H
+  .ellipseTo([2.95, -12.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([2.95, -12.25]) // v
+  .ellipseTo([2.892, -12.108], 0.2, 0.2, 0, false, true) // A
+  .lineTo([1.892, -11.108]) // l
+  .ellipseTo([1.75, -11.05], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-1.75, -11.05]) // h
+  .ellipseTo([-1.892, -11.108], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-2.892, -12.108]) // l
+  .ellipseTo([-2.95, -12.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-2.95, -15.75]) // v
+  .ellipseTo([-2.892, -15.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-2.369, -16.414]) // l
+  .cubicBezierCurveTo([-7.651, -14.69], [-3.959, -16.193], [-6.199, -15.484]) // C
+  .lineTo([-6.01, -11.859]) // l
+  .cubicBezierCurveTo([-7.222, -11.157], [-5.626, -11.197], [-6.835, -10.496]) // c
+  .lineTo([-8.871, -13.976]) // l
+  .cubicBezierCurveTo([-13.991, -8.874], [-10.708, -12.852], [-12.872, -10.706]) // c
+  .lineTo([-11.151, -7.264]) // l
+  .cubicBezierCurveTo([-11.849, -6.05], [-10.484, -6.885], [-11.182, -5.671]) // c
+  .lineTo([-14.696, -7.668]) // l
+  .cubicBezierCurveTo([-16.396, -2.717], [-15.497, -6.223], [-16.18, -4.008]) // c
+  .ellipseTo([-16.626, -2.553], 0.2, 0.2, 0, false, true) // a
+    .ellipseTo([-16.79, -2.783], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([-14.963, -8.052], [-16.557, -4.181], [-15.847, -6.54]) // c
+  .ellipseTo([-14.691, -8.125], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-11.651, -6.397]) // l
+  .cubicBezierCurveTo([-11.349, -6.916], [-11.449, -6.282], [-11.146, -6.801]) // c
+  .lineTo([-14.391, -8.641]) // l
+  .ellipseTo([-14.465, -8.916], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([-8.915, -14.447], [-13.297, -10.917], [-10.916, -13.275]) // c
+  .ellipseTo([-8.641, -14.375], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-6.877, -11.359]) // l
+  .cubicBezierCurveTo([-6.356, -11.658], [-6.759, -11.158], [-6.239, -11.456]) // c
+  .lineTo([-8.11, -14.686]) // l
+  .ellipseTo([-8.037, -14.959], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([-1.762, -16.949], [-6.36, -15.919], [-3.476, -16.852]) // c
+  .ellipseTo([-1.608, -16.608], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-2.55, -15.667]) // l
+  .lineTo([-2.55, -12.333]) // v
+  .lineTo([-1.667, -11.45]) // l
+  .lineTo([1.667, -11.45]) // h
+  .lineTo([2.55, -12.333]) // l
+  .lineTo([2.55, -12.55]) // v
+  .lineTo([-1.25, -12.55]) // H
+  .ellipseTo([-1.45, -12.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-1.45, -13.25]) // v
+  .ellipseTo([-1.25, -13.45], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.45, -13.45]) // h
+  .lineTo([-0.45, -14.55]) // v
+  .lineTo([-0.75, -14.55]) // H
+  .ellipseTo([-0.95, -14.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.95, -15.25]) // v
+  .ellipseTo([-0.75, -15.45], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.75, -15.45]) // h
+  .ellipseTo([0.95, -15.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.95, -14.75]) // v
+  .ellipseTo([0.75, -14.55], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.45, -14.55]) // H
+  .lineTo([0.45, -13.45]) // V
+  .lineTo([2.167, -13.45]) // H
+  .lineTo([2.55, -13.833]) // l
+  .lineTo([2.55, -15.668]) // v
+  .lineTo([1.707, -16.519]) // l
+  .ellipseTo([1.847, -16.859], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([4.797, -16.281], [2.489, -16.866], [3.609, -16.633]) // c
+    .cubicBezierCurveTo([8.04, -14.963], [5.984, -15.929], [7.22, -15.46]) // c
+  .ellipseTo([8.11, -14.692], 0.2, 0.2, 0, false, true) // a
+  .lineTo([6.388, -11.685]) // l
+  .cubicBezierCurveTo([6.907, -11.384], [6.272, -11.482], [6.791, -11.181]) // c
+  .lineTo([8.631, -14.393]) // l
+  .ellipseTo([8.907, -14.464], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([14.456, -8.956], [11.039, -13.181], [13.273, -10.876]) // c
+  .ellipseTo([14.387, -8.679], 0.2, 0.2, 0, false, true) // a
+  .lineTo([11.306, -6.885]) // l
+  .cubicBezierCurveTo([11.604, -6.364], [11.104, -6.767], [11.403, -6.247]) // c
+  .lineTo([14.689, -8.158]) // l
+  .ellipseTo([14.964, -8.086], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([16.311, -4.727], [15.47, -7.208], [15.955, -5.926]) // c
+    .cubicBezierCurveTo([16.858, -1.853], [16.666, -3.527], [16.897, -2.439]) // c
+  .ellipseTo([16.541, -1.704], 0.2, 0.2, 0, false, true) // A
+  .cubicBezierCurveTo([15.995, -2.216], [16.407, -1.802], [16.201, -2.01]) // C
+    .cubicBezierCurveTo([15.667, -2.55], [15.821, -2.39], [15.712, -2.504]) // C
+  .lineTo([12.333, -2.55]) // H
+  .lineTo([11.45, -1.667]) // L
+  .lineTo([11.45, -0.95]) // V
+  .lineTo([12.167, -0.95]) // h
+  .lineTo([12.608, -1.392]) // l
+  .ellipseTo([12.75, -1.45], 0.2, 0.2, 0, false, true) // A
+  .lineTo([15.25, -1.45]) // h
+  .ellipseTo([15.45, -1.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([15.45, -0.75]) // v
+  .ellipseTo([15.25, -0.55], 0.2, 0.2, 0, false, true) // A
+  .lineTo([13.333, -0.55]) // H
+  .lineTo([12.95, -0.167]) // L
+  .lineTo([12.95, 0.167]) // V
+  .lineTo([13.333, 0.55]) // L
+  .lineTo([15.25, 0.55]) // H
+  .ellipseTo([15.45, 0.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([15.45, 1.25]) // v
+  .ellipseTo([15.25, 1.45], 0.2, 0.2, 0, false, true) // A
+  .lineTo([12.75, 1.45]) // h
+  .ellipseTo([12.608, 1.392], 0.2, 0.2, 0, false, true) // a
+  .lineTo([12.167, 0.95]) // l
+  .lineTo([11.45, 0.95]) // h
+  .lineTo([11.45, 1.667]) // v
+  .lineTo([12.333, 2.55]) // l
+  .lineTo([15.667, 2.55]) // h
+  .lineTo([16.559, 1.65]) // l
+  .ellipseTo([16.9, 1.785], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([16.766, 2.849], [16.908, 2.01], [16.857, 2.37]) // c
+    .cubicBezierCurveTo([16.366, 4.528], [16.674, 3.327], [16.538, 3.911]) // c
+    .cubicBezierCurveTo([14.951, 8.061], [16.022, 5.764], [15.537, 7.131]) // c
+  .ellipseTo([14.682, 8.127], 0.2, 0.2, 0, false, true) // a
+  .lineTo([11.695, 6.391]) // l
+  .cubicBezierCurveTo([11.396, 6.911], [11.494, 6.273], [11.194, 6.794]) // c
+  .lineTo([14.38, 8.647]) // l
+  .ellipseTo([14.45, 8.925], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([8.967, 14.448], [13.216, 10.942], [10.941, 13.261]) // c
+  .ellipseTo([8.691, 14.377], 0.2, 0.2, 0, false, true) // a
+  .lineTo([6.943, 11.367]) // l
+  .cubicBezierCurveTo([6.424, 11.666], [6.826, 11.165], [6.307, 11.464]) // c
+  .lineTo([8.172, 14.677]) // l
+  .ellipseTo([8.096, 14.952], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([1.813, 16.892], [6.511, 15.834], [3.867, 16.747]) // C
+  .ellipseTo([1.626, 16.593], 0.2, 0.2, 0, false, true) // a
+  .cubicBezierCurveTo([1.711, 16.484], [1.656, 16.539], [1.677, 16.521]) // c
+    .cubicBezierCurveTo([1.833, 16.355], [1.745, 16.447], [1.786, 16.403]) // c
+    .cubicBezierCurveTo([2.161, 16.033], [1.927, 16.259], [2.046, 16.144]) // c
+    .cubicBezierCurveTo([2.55, 15.664], [2.359, 15.842], [2.5, 15.711]) // c
+  .lineTo([2.55, 12.333]) // v
+  .lineTo([1.667, 11.45]) // l
+  .lineTo([0.45, 11.45]) // H
+  .lineTo([0.45, 13.167]) // v
+  .lineTo([0.892, 13.608]) // L
+  .ellipseTo([0.95, 13.75], 0.2, 0.2, 0, false, true) // A
+  .lineTo([0.95, 14.25]) // v
+  .ellipseTo([0.892, 14.392], 0.2, 0.2, 0, false, true) // a
+  .lineTo([0.392, 14.892]) // l
+  .ellipseTo([0.25, 14.95], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.25, 14.95]) // h
+  .ellipseTo([-0.392, 14.892], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-0.892, 14.392]) // l
+  .ellipseTo([-0.95, 14.25], 0.2, 0.2, 0, false, true) // A
+  .lineTo([-0.95, 13.75]) // v
+  .ellipseTo([-0.892, 13.608], 0.2, 0.2, 0, false, true) // a
+  .lineTo([-0.45, 13.167]) // l
+  .lineTo([-0.45, 11.45]) // v
+  .lineTo([-1.667, 11.45]) // H
+  .lineTo([-2.55, 12.333]) // l
+  .lineTo([-2.55, 15.666]) // v
+  .lineTo([-1.643, 16.558]) // l
+  .ellipseTo([-1.794, 16.9], 0.2, 0.2, 0, false, true) // a
+  .close(); // z
